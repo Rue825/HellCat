18699e3084e1af681ad73d276fbbe32b17da4abb
Rue825 <44816774+Rue825@users.noreply.github.com>
2018-11-10 18:38:58 +0300

    Add files via upload

--- article.php
+++ article.php
@@ -1,36 +1,106 @@
 <?php
   require "db.php";
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
 	<?php
+		global $id;
+		global $id_s;
 		require_once "functions/functions.php";
 		$news = getNews (1, $_GET["id"]);
 		$title = $news["title"];
-		require_once "blocks/head.php"; 
+
+
+		require_once "blocks/head.php";
 	?>
-</head>
-<body>
-	<?php 
-		require_once "blocks/header.php"; 
-	?>
- 	<div id="wrapper">
- 		<div id="leftCol">
- 		<?php
- 			echo '<div id=\"bigArticle\">
- 			<img src="/img/articles/'.$news["id"].'.jpg" alt="Статья '.$news["id"].'" title="Статья '.$news["id"].'">
- 				<h2>'.$news["title"].'</h2>
- 				<p>'.$news["full_text"].'</p>
- 			</div>';
+	<body class="is-preload">
+
+		<!-- Header -->
+		<?php
+			require_once "blocks/header.php"; 
+		?>
+
+		<!-- One -->
+<!-- 		<section id="one" class="main style4 right dark fullscreen">
+			<div class="content box style4">
+				<img src="img/9.jpg" width="70%" height="70%">
+				<header>
+					<h2>What I Do</h2>
+				</header>
+				<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+				Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis arcu,
+				id varius justo euismod in. Curabitur egestas consectetur magna.</p>
+			</div>
+		</section>		 -->
+		<section id="one" class="main style4 right dark fullscreen">
+		<div class="content box style4">		
+	    <?php
+	    	$data = $_POST;
+	    	if ($news['date_edit'] == "0000-00-00")
+ 			{
+ 				$news['date_edit'] = "";
+ 			}
+ 			if ($news['time_edit'] == "00:00:00")
+ 			{
+ 				$news['time_edit'] = "";
+ 			}
+ 		 	echo '
+ 					<img src="/img/articles/'.$news["id"].'.jpg" alt="Статья '.$news["id"].'" title="Статья '.$news["id"].'" width="70%" height="70%">
+ 					<header>
+ 						<h2>'.$news["title"].'</h2>
+ 					</header>
+ 						<p>Дата публикации: '.$news["date_add"].' / '.$news["time_add"].'</p>
+ 						<p>Дата изменения: '.$news["date_edit"].' / '.$news["time_edit"].'</p>
+ 						<p>Автор: '.$news["author"].'</p>
+ 						<p>'.$news["full_text"].'</p>
+ 						<hr/>
+ 						<p>Последние комментарии:</p>
+ 						<hr/>
+ 				';
+
+
+ 			$comment = R::findAll('comment', "ORDER BY `id` DESC"); 
+ 			foreach ($comment as $comments) 
+ 			{ 
+ 					echo '<h1>Дата создания: '.$comments['date_add'].' / '.$comments['time_add'].'
+ 						  <img src="img/logo.jpg" width = "120" height = "120" align="left"/>
+ 						  <h1>Автор: '.$comments['name'].', почта: ' .$comments['email'].'					  
+ 						  </h1>
+ 						  </h1>
+ 						  <p>'.$comments['message'].'</p>
+						  <hr/>
+ 					';	
+ 			}
+ 			if ((isset($_SESSION['logged_user']) && ($_SESSION['logged_user']['auth_key']==1 || ($_SESSION['logged_user']['auth_key']==0))))
+ 			{	 	
+		    	echo '<form method="POST" action="/addcomment.php"
+		    		  <a href="/addcomment.php" style="color:white">
+		    		  <input type="submit" value="Добавить комментарий" />
+		    		  </a>
+		    		  </form>
+		    		  ';
+ 			}
+ 			else
+ 			{
+ 				echo 'Для того, чтобы добавить комментарий, необходимо <a href="auth.php">авторизоваться.</a>';
+ 			}
  		?>
  		</div>
- 		<?php 
- 			require_once "blocks/rightCol.php"; 
- 		?>
- 	</div>
- 	<?php 
- 		require_once "blocks/footer.php"; 
- 	?>
-</body>
+ 		</section>
+
+		<!-- Footer -->
+		<?php
+			require_once "blocks/footer.php"; 
+		?>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
 </html>
